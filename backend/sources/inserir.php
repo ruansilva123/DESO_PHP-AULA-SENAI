24d5b8f3fd49ecf7be9ba8e6c15ea598a3fd2c97
ruansilva123 <ruancplay59@gmail.com>
2024-05-21 17:15:12 -0300

    first commit

---
+++ backend/sources/inserir.php
@@ -0,0 +1,23 @@
+<?php
+    include_once("conexao.php");
+
+    $email = $_POST["email"];
+    $senha = MD5($_POST["senha"]);
+
+    $sqlVerification = $conn -> prepare("SELECT pk_usuario, email_usuario, senha_usuario, eh_adm_usuario FROM usuario WHERE email_usuario = ?");
+
+    $sqlVerification -> execute([$email]);
+
+    if($sqlVerification -> rowCount() == 0){
+        $sql = $conn -> prepare("INSERT INTO usuario (email_usuario, senha_usuario) VALUES (?,?)");
+        $sql -> execute([$email, $senha]);
+
+        header("location:../users_pages/cadastro.php");
+    }else{
+        echo("<H1>O e-mail informado já está cadastrado!</H1>");
+    }
+
+    unset($sqlVerification);
+    unset($sql);
+    unset($conn);
+?>
