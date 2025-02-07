612bf0eb14534b9776c634894d24e990d2ae3ea9
RuanCarlosSilva <143547755+ruansilva123@users.noreply.github.com>
2025-02-07 16:12:03 -0300

    :bug: fix: correção de bugs

--- src/pages/edit.php
+++ src/pages/edit.php
@@ -9,10 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../static/style/styles.csss">
+    <link rel="stylesheet" href="../../static/style/styles.css">
 </head>
 
-<body class="d-flex justify-content-center align-items-center">
+<body class="d-flex justify-content-center align-items-center" style="heigth=100vh">
     <main class="col-6">
         <div class="row p-5 border rounded-2 blur">
             <h1 class="mb-4 d-flex justify-content-center">Edit</h1>
--- src/pages/home.php
+++ src/pages/home.php
@@ -16,38 +16,50 @@
     <link rel="stylesheet" href="../../static/style/styles.css">
 </head>
 <body>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid justify-content-start">
-            <button class="btn btn-outline-success me-2" type="button"><a href="login.php" style="text-decoration:none">Logout</a></button>
-            <button class="btn btn-outline-success me-2" type="button"><a href="register.php" style="text-decoration:none">Register new user</a></button>
+    <nav class="navbar navbar-blur">
+        <div class="container-fluid justify-content-end">
+            <button class="btn btn-primary me-2" type="button"><a href="login.php" style="text-decoration:none; color:inherit">Logout</a></button>
+            <button class="btn btn-primary me-2" type="button"><a href="register.php" style="text-decoration:none; color:inherit">Register new user</a></button>
         </div>
     </nav>
-    <table>
-        <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Email</th>
-            <th scope="col">Is admin</th>
-            <th scope="col">Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-            include_once("../sources/user/listAll.php");
-            if(!empty($users)){
-            foreach($users as $newUser){
-                echo ' <tr>
-                        <td>' . $newUser["pk_user"] . '</td>
-                        <td>' . $newUser["user_email"] . '</td>
-                        <td>' . $newUser["is_admin"] . '</td>
-                        <td><a href="../sources/user/exclude.php?user='.$newUser["pk_user"].'">Exclude</a></td>  
-                        <td><a href="edit.php?user='.$newUser["pk_user"].'">Edit</a></td>  
-                    </tr>';
-            }
-            }
-        ?>
-        </tbody>
-    </table>
+    <div class="row justify-content-center mt-5">
+        <div class="col-6">
+            <table class="table table-secondary table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Is admin</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        include_once("../sources/user/listAll.php");
+                        if(!empty($users)){
+                        foreach($users as $newUser){
+                            echo ' <tr>
+                                    <td>' . $newUser["pk_user"] . '</td>
+                                    <td>' . $newUser["user_email"] . '</td>
+                                    <td>' . $newUser["is_admin"] . '</td>
+                                    <td><a
+                                        class="btn btn-primary"
+                                        style="text-decoration:none; color:inherit; color:white"
+                                        href="../sources/user/exclude.php?user='.$newUser["pk_user"].'">Exclude</a>
+                                    </td>
+                                    <td><a
+                                        class="btn btn-primary"
+                                        style="text-decoration:none; color:inherit; color:white" 
+                                        href="edit.php?user='.$newUser["pk_user"].'">Edit</a>
+                                    </td>
+                                </tr>';
+                        }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
